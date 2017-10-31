49ae54b75baad5f372677eeffd27a214aed3967a
Justyna Rachowicz <mrowa44@gmail.com>
2017-10-31 17:32:48 +0100

    Add posts query

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -17,4 +17,11 @@ Types::QueryType = GraphQL::ObjectType.define do
     description 'Find a Post by ID'
     resolve ->(_obj, args, _ctx) { Post.find(args['id']) }
   end
+
+  field :posts do
+    type types[Types::PostType]
+    argument :limit, types.Int
+    description 'List all posts'
+    resolve ->(_obj, args, _ctx) { Post.first(args[:limit] || 100) }
+  end
 end
